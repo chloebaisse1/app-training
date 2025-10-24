b6b98625cb68ef6d10a29ef71ef55940355ba85a
chloebaisse1 <baissechloe@gmail.com>
2025-10-24 11:03:34 +0400

    creation du formulaire de connexion (style temporaire) en html

--- Forms/login.php
+++ Forms/login.php
@@ -1,11 +1,11 @@
 <?php
-
-// require_once __DIR__ =
-//constante qui contient le chemin absolu du dossier dans lequel se trouve le fichier courant.
-
 session_start(); // demarrer la session
 require_once __DIR__ . '/../Classes/User.php';
 require_once __DIR__ . '/../db.php';
+
+
+// require_once __DIR__ =
+//constante qui contient le chemin absolu du dossier dans lequel se trouve le fichier courant.
 
 $message = "";
 
@@ -23,7 +23,6 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $conn = $db->connect();
     $stmt = $conn->prepare("SELECT id FROM users WHERE email  = ?");
     $stmt->execute([$email]);
-
     // data = données de l'utilisateur (ID)
     $userData = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -39,3 +38,40 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
   }
 }
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Connexion</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+
+<div class="container mt-5">
+    <div class="col-md-6 offset-md-3">
+        <div class="card shadow p-4">
+            <h3 class="text-center mb-3">Connexion</h3>
+
+        <?php if($message): ?>
+          <div class="alert alert-info"><?= htmlspecialchars($message) ?></div>
+          <?php endif; ?>
+
+
+      <form method="POST" novalidate>
+        <div class="mb-3">
+          <label for="email" class="form-label">Email</label>
+          <input type="email" name="email" class="form-control" required value="<?= htmlspecialchars($email ?? '') ?>">
+        </div>
+
+        <div class="mb-3">
+          <label for="password" class="form-label">Mot de passe</label>
+          <input type="password" class="form-control" id="password" name="password" required>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Se connecter</button>
+      </form>
+      </div>
+    </div>
+</div>
+</body>
+</html>
