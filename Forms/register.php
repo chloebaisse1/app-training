4c557ed810fdd66aff322838c9f28147121bce68
chloebaisse1 <baissechloe@gmail.com>
2025-10-24 10:51:56 +0400

    creation du formulaire d'inscription(style temporaire) en html, ajout de bootstrap

--- Forms/register.php
+++ Forms/register.php
@@ -53,6 +53,7 @@ if(isset($_GET['success'])){
   <title>Inscription</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body class="bg-light">
   <div class="container mt-5">
     <div class="col-md-6 offset-md-3">
